1bbe9df23c0df250d849c239a538d64f6e8cf352
Its-Soumya <soumyadsw@gmail.com>
2024-11-17 15:31:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-const apiKey = "4b006fc9f4ce5ad6a645c7d6e9b50958"
+const apiKey = "4b006fc9f4ce5ad6a645c7d6e9b50958";
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
 const searchBox = document.querySelector(".search input");
